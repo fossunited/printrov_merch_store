e6024c2a94937dff0bc81de49c94e489c3000fcc
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-10-24 14:53:31 +0530

    feat: allow custom product template

--- printrov_merch_store/printrov_merch_store/doctype/store_product/store_product.py
+++ printrov_merch_store/printrov_merch_store/doctype/store_product/store_product.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2023, Build With Hussain and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class StoreProduct(WebsiteGenerator):
-	pass
+	def get_context(self, context):
+		settings = frappe.get_cached_doc("Printrove Settings")
+
+		if settings.use_custom_product_view_template:
+			custom_rendered_html = frappe.render_template(
+				settings.custom_product_view_template, 
+				{"doc": self}
+			)
+
+			context.custom_rendered_html = custom_rendered_html
+
+
