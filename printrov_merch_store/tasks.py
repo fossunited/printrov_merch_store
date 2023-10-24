a2b06e475ce20555a6e26a29ad2530dcf92463ad
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-10-24 14:46:51 +0530

    perf: cache the token for a year
    * although it is apparently valid for 2 years, yet to be confirmed

--- printrov_merch_store/tasks.py
+++ printrov_merch_store/tasks.py
@@ -4,6 +4,7 @@ from frappe.integrations.utils import make_get_request, make_post_request
 
 
 BASE_URL = "https://api.printrove.com/"
+SECONDS_IN_YEAR = 364 * 24 * 60 * 60
 
 @frappe.whitelist()
 def sync_products_from_printrove():
@@ -35,7 +36,11 @@ def sync_products_from_printrove():
             doc.save(ignore_permissions=True)
 
 def get_printrove_access_token():
-    printrove_settings = frappe.get_single("Printrove Settings")
+    token = frappe.cache.get_value("printrove_access_token")
+    if token:
+        return token
+
+    printrove_settings = frappe.get_cached_doc("Printrove Settings")
     auth_route = "api/external/token"
     response = make_post_request(
         f"{BASE_URL}{auth_route}",
@@ -44,4 +49,10 @@ def get_printrove_access_token():
             "password": printrove_settings.get_password("password"),
         },
     )
-    return response["access_token"]
+
+    access_token = response["access_token"]
+
+    # store for a year
+    frappe.cache.set_value("printrove_access_token", access_token, expires_in_sec=SECONDS_IN_YEAR)
+
+    return access_token
