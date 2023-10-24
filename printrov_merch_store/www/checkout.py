0a15c9ae714096dd287900859a8edff4bd31f41c
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-10-24 15:44:22 +0530

    feat: checkout page
    * protected!

---
+++ printrov_merch_store/www/checkout.py
@@ -0,0 +1,6 @@
+import frappe
+
+def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.local.flags.redirect_location = f"/login?redirect-to={frappe.request.url}"
+        raise frappe.Redirect
