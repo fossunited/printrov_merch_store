f13209efb430e56d47e20df58de1cbffe9812c96
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-10-25 15:09:54 +0530

    feat: variants in store product

--- printrov_merch_store/tasks.py
+++ printrov_merch_store/tasks.py
@@ -12,20 +12,19 @@ SECONDS_IN_YEAR = 364 * 24 * 60 * 60
 
 @frappe.whitelist()
 def sync_products_from_printrove():
-    access_token = get_printrove_access_token()
-    headers = {"Authorization": f"Bearer {access_token}"}
     products_route = "api/external/products"
-    all_products = make_get_request(
-        f"{BASE_URL}{products_route}", headers=headers
-    )
+    all_products = make_printrove_request(products_route)
     all_products = all_products["products"]
 
     for product in all_products:
+        variants = get_product_variants(product["id"])
+
         product_data = {
             "front_mockup": product["mockup"]["front_mockup"],
             "back_mockup": product["mockup"]["back_mockup"],
             "printrove_category": product["product"]["name"],
             "printrove_category_id": product["product"]["id"],
+            "variants": variants,
         }
 
         if not frappe.db.exists(
@@ -46,6 +45,28 @@ def sync_products_from_printrove():
             )
             doc.update({**product_data})
             doc.save(ignore_permissions=True)
+
+
+def get_product_variants(product_id):
+    product_details_endpoint = f"api/external/products/{product_id}"
+    product_details = make_printrove_request(product_details_endpoint)
+
+    variants = product_details["product"]["variants"]
+
+    # process variants
+    processed_variants = []
+    for variant in variants:
+        processed_variants.append(
+            {
+                "variant_id": variant["id"],
+                "sku": variant["sku"],
+                "variant_name": variant["product"]["name"],
+                "color": variant["product"]["color"],
+                "size": variant["product"]["size"],
+            }
+        )
+
+    return processed_variants
 
 
 def get_printrove_access_token():
@@ -73,3 +94,14 @@ def get_printrove_access_token():
     )
 
     return access_token
+
+
+def make_printrove_request(endpoint, method="GET"):
+    access_token = get_printrove_access_token()
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    make_request = (
+        make_get_request if method == "GET" else make_post_request
+    )
+    response = make_request(f"{BASE_URL}{endpoint}", headers=headers)
+    return response
