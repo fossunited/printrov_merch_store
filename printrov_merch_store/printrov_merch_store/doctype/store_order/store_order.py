c4dd7251f47c1aed76e899cd8eecc8b38f317aa8
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-10-26 12:37:50 +0530

    feat: printrove order start
    END of part 5!

--- printrov_merch_store/printrov_merch_store/doctype/store_order/store_order.py
+++ printrov_merch_store/printrov_merch_store/doctype/store_order/store_order.py
@@ -1,9 +1,54 @@
 # Copyright (c) 2023, Build With Hussain and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+
+from printrov_merch_store.utils import make_printrove_request
 
 
 class StoreOrder(Document):
-    pass
+    def on_update(self):
+        if self.has_value_changed("status") and self.status == "Paid":
+            frappe.enqueue_doc(
+                "Store Order",
+                self.name,
+                "place_order_on_printrove",
+                queue="short",
+            )
+
+    def place_order_on_printrove(self):
+        order_endpoint = "api/external/orders"
+
+        printrove_product_id = frappe.db.get_value(
+            "Store Product", self.product, "printrove_id"
+        )
+        order_payload = {
+            "reference_number": self.name,
+            "retail_price": self.retail_price,
+            "customer": {
+                "name": self.customer_name,
+                "email": self.user,
+                "number": self.phone_number,
+                "address1": self.address_line_1,
+                "address2": self.address_line_2,
+                "address3": self.address_line_3,
+                "city": self.city,
+                "state": self.state,
+                "country": self.country,
+                "pincode": self.pincode,
+            },
+            "order_products": [
+                {
+                    "product_id": printrove_product_id,
+                    "variant_id": self.variant_id,
+                    "quantity": 1,  # TODO: later, bulk!
+                }
+            ],
+        }
+
+        response = make_printrove_request(
+            order_endpoint, method="POST", data=order_payload
+        )
+
+        breakpoint()
--- printrov_merch_store/tasks.py
+++ printrov_merch_store/tasks.py
@@ -1,11 +1,6 @@
 import frappe
-from frappe.integrations.utils import (
-    make_get_request,
-    make_post_request,
-)
 
-BASE_URL = "https://api.printrove.com/"
-SECONDS_IN_YEAR = 364 * 24 * 60 * 60
+from printrov_merch_store.utils import make_printrove_request
 
 
 @frappe.whitelist()
@@ -65,41 +60,3 @@ def get_product_variants(product_id):
         )
 
     return processed_variants
-
-
-def get_printrove_access_token():
-    token = frappe.cache.get_value("printrove_access_token")
-    if token:
-        return token
-
-    printrove_settings = frappe.get_cached_doc("Printrove Settings")
-    auth_route = "api/external/token"
-    response = make_post_request(
-        f"{BASE_URL}{auth_route}",
-        data={
-            "email": printrove_settings.email,
-            "password": printrove_settings.get_password("password"),
-        },
-    )
-
-    access_token = response["access_token"]
-
-    # store for a year
-    frappe.cache.set_value(
-        "printrove_access_token",
-        access_token,
-        expires_in_sec=SECONDS_IN_YEAR,
-    )
-
-    return access_token
-
-
-def make_printrove_request(endpoint, method="GET"):
-    access_token = get_printrove_access_token()
-    headers = {"Authorization": f"Bearer {access_token}"}
-
-    make_request = (
-        make_get_request if method == "GET" else make_post_request
-    )
-    response = make_request(f"{BASE_URL}{endpoint}", headers=headers)
-    return response
