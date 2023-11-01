2932f4fd1427156b37ba9c7d782c807c51a8abdf
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-11-01 23:08:02 +0530

    feat: publish/unpublish button in store product

--- printrov_merch_store/printrov_merch_store/doctype/store_product/store_product.js
+++ printrov_merch_store/printrov_merch_store/doctype/store_product/store_product.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2023, Build With Hussain and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Store Product", {
-// 	refresh(frm) {
+frappe.ui.form.on("Store Product", {
+  refresh(frm) {
+    const button_label = frm.doc.is_published ? __("Unpublish") : __("Publish");
 
-// 	},
-// });
+    frm.add_custom_button(button_label, () => {
+      frm.set_value("is_published", !frm.doc.is_published);
+      frm.save();
+    });
+  },
+});
