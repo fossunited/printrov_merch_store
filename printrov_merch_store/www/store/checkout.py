dfd0649f0f9a4965c20c4814b79260e35eeb9754
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-11-08 22:37:39 +0530

    feat: better success page, also custom success page
    Closes #15

--- printrov_merch_store/www/store/checkout.py
+++ printrov_merch_store/www/store/checkout.py
@@ -7,3 +7,10 @@ def get_context(context):
             f"/login?redirect-to={frappe.request.url}"
         )
         raise frappe.Redirect
+
+    success_page_route = "/store/success"
+    printrove_settings = frappe.get_cached_doc("Printrove Settings")
+    if printrove_settings.override_success_page:
+        success_page_route = printrove_settings.success_page_route
+
+    context.success_page_route = success_page_route
