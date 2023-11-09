549be49ff2d88f46d94230aeb01d1ccc23280406
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-11-09 15:39:11 +0530

    fix(ux): make sign up page default
    Fixes #21

--- printrov_merch_store/www/store/checkout.py
+++ printrov_merch_store/www/store/checkout.py
@@ -4,7 +4,7 @@ import frappe
 def get_context(context):
     if frappe.session.user == "Guest":
         frappe.local.flags.redirect_location = (
-            f"/login?redirect-to={frappe.request.url}"
+            f"/login?redirect-to={frappe.request.url}#signup"
         )
         raise frappe.Redirect
 
